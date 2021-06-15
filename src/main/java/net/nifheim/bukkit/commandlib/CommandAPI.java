70fb3eb652ef6e803ec3b0315c17a32b1202aa07
Beelzebu <jsuarez.lara@outlook.com>
2021-06-14 22:10:33 -0400

    Added basic 1.8 support

--- src/main/java/net/nifheim/bukkit/commandlib/CommandAPI.java
+++ src/main/java/net/nifheim/bukkit/commandlib/CommandAPI.java
@@ -1,13 +1,12 @@
 package net.nifheim.bukkit.commandlib;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-import java.util.Set;
+import java.util.logging.Level;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
@@ -15,12 +14,21 @@ import org.bukkit.plugin.Plugin;
 
 /**
  * @author Beelzebu
- * TODO: add support for 1.8+
  */
 public class CommandAPI {
 
     private static final Map<Plugin, List<Command>> REGISTERED_COMMANDS = new HashMap<>();
     private static CommandMap commandMap;
+
+    static {
+        try {
+            Field commandMapField = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            commandMapField.setAccessible(true);
+            commandMap = (CommandMap) commandMapField.get(Bukkit.getServer());
+        } catch (NoSuchFieldException | IllegalAccessException ex) {
+            Bukkit.getLogger().log(Level.SEVERE, "Failed to get command map: ", ex);
+        }
+    }
 
     private CommandAPI() {
     }
@@ -28,14 +36,6 @@ public class CommandAPI {
     public static void unregister(Plugin plugin) {
         Iterator<Command> commandIterator = REGISTERED_COMMANDS.get(plugin).iterator();
         commandIterator.forEachRemaining(command -> unregisterCommand(plugin, command));
-        Iterator<Map.Entry<String, Command>> knownCommandsIterator = new HashSet<>(getCommandMap().getKnownCommands().entrySet()).iterator();
-        knownCommandsIterator.forEachRemaining(entry -> {
-            String name = entry.getKey();
-            Command command = entry.getValue();
-            if (name.startsWith(plugin.getName().toLowerCase(Locale.ENGLISH))) {
-                unregisterCommand(plugin, command);
-            }
-        });
     }
 
     public static void registerCommand(Plugin plugin, Command command) {
@@ -54,33 +54,11 @@ public class CommandAPI {
         if (bukkitCommand != null) {
             bukkitCommand.unregister(getCommandMap());
         }
-        Map<String, Command> knownCommands = getKnownCommandsMap();
-        Set<String> toRemove = new HashSet<>();
         command.setLabel(command.getName());
-        for (Map.Entry<String, Command> entry : knownCommands.entrySet()) {
-            String name = entry.getKey();
-            Command registeredCommand = entry.getValue();
-            if (name.equalsIgnoreCase(command.getName()) || name.startsWith(plugin.getName().toLowerCase(Locale.ROOT))) {
-                registeredCommand.unregister(getCommandMap());
-                toRemove.add(name);
-            }
-        }
-        toRemove.forEach(getKnownCommandsMap()::remove);
-        command.getAliases().forEach(getKnownCommandsMap()::remove);
-        getKnownCommandsMap().remove(plugin.getName() + ":" + command.getName().toLowerCase(Locale.ENGLISH).trim());
     }
 
     private static CommandMap getCommandMap() {
-        if (commandMap != null) {
-            return commandMap;
-        }
-        return commandMap = Bukkit.getCommandMap();
-    }
-
-
-    @SuppressWarnings("unchecked")
-    private static Map<String, Command> getKnownCommandsMap() {
-        return getCommandMap().getKnownCommands();
+        return commandMap;
     }
 }
 
